45a6eea02fb11dd25d0e843aaf1eae8df4818d92
Ilia Choly <ilia.choly@gmail.com>
2018-09-28 20:04:59 -0400

    start the parser

---
+++ parser/parser.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"github.com/icholy/monkey/ast"
+	"github.com/icholy/monkey/lexer"
+	"github.com/icholy/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	return &Parser{}
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
