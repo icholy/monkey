48321cf1d37283592ad99713a878c26220d4a716
Ilia Choly <ilia.choly@gmail.com>
2018-09-28 20:04:53 -0400

    start the ast package

---
+++ ast/ast.go
@@ -0,0 +1,51 @@
+package ast
+
+import (
+	"github.com/icholy/monkey/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) == 0 {
+		return ""
+	}
+	return p.Statements[0].TokenLiteral()
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (LetStatement) statementNode() {}
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
+}
