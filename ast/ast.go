e42757aff043f4be3a2ae82801387e726984ac5e
Ilia Choly <ilia.choly@gmail.com>
2018-09-28 21:16:55 -0400

    add String methods to add Node implementations

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,15 @@
 package ast
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/icholy/monkey/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -22,6 +26,14 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) String() string {
+	var b strings.Builder
+	for _, s := range p.Statements {
+		fmt.Fprintln(&b, s)
+	}
+	return b.String()
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) == 0 {
 		return ""
@@ -32,6 +44,10 @@ func (p *Program) TokenLiteral() string {
 type Identifier struct {
 	Token token.Token
 	Value string
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
 
 func (Identifier) expressionNode() {}
@@ -45,6 +61,10 @@ type LetStatement struct {
 	Value Expression
 }
 
+func (l *LetStatement) String() string {
+	return fmt.Sprintf("let %s = %s;", l.Name, l.Value)
+}
+
 func (LetStatement) statementNode() {}
 func (l *LetStatement) TokenLiteral() string {
 	return l.Token.Literal
@@ -55,7 +75,23 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
-func (r *ReturnStatement) statementNode() {}
+func (r *ReturnStatement) String() string {
+	return fmt.Sprintf("return %s;", r.ReturnValue)
+}
+func (ReturnStatement) statementNode() {}
 func (r *ReturnStatement) TokenLiteral() string {
 	return r.Token.Literal
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (e *ExpressionStatement) String() string {
+	return e.Expression.String()
+}
+func (ExpressionStatement) statementNode() {}
+func (e *ExpressionStatement) TokenLiteral() string {
+	return e.Token.String()
 }
